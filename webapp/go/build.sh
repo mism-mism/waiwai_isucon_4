4d6343ee69e83e365b12f1af38ede0cf18569622
MasamiMishima <mishima0304@gmail.com>
2019-04-07 14:28:01 +0900

    add docker

--- webapp/go/build.sh
+++ webapp/go/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-go get github.com/google/pprof
+ # go get github.com/google/pprof
 go get github.com/go-martini/martini
 go get github.com/go-sql-driver/mysql
 go get github.com/martini-contrib/render
