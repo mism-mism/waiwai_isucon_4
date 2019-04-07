9e3894c534f5d6f0ac4e2a6d8121c6cf4649642a
Okada Shohei <okashoicircus0@gmail.com>
2019-04-07 14:59:42 +0900

    先にログを消す

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,12 @@ exit
 cd webapp/go
 ./build.sh
 
-/etc/init.d/mysqld restart
-/etc/init.d/redis restart
-/etc/init.d/nginx restart
-/etc/init.d/supervisord restart
-
 rm /var/log/mysqld.log
 rm /var/log/nginx/*
 rm /var/log/redis/*
 rm /tmp/isucon.go.log
+
+/etc/init.d/mysqld restart
+/etc/init.d/redis restart
+/etc/init.d/nginx restart
+/etc/init.d/supervisord restart
